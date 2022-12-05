0c269e22cbcee3f775ca034fa0304c2a98e5edb5
kev1nweng <95159615+kev1nweng@users.noreply.github.com>
2022-12-05 16:46:12 +0800

    update 0.2.1

--- Escape.py
+++ Escape.py
@@ -1,15 +1,15 @@
 # 此程序为Windows设计，在其它操作系统上会出现兼容性问题。
 
 from utils.shortcuts import *
-
-from utils.utils import *
+from utils.imports import *
+from utils.functions import *
 
 # 防止 Ctrl+C Traceback 并退出
 def signal_handler(signal,frame):
 
     print('\n\n[!] 你按下了Ctrl+C，正在退出程序...')
     time.sleep(1)
-    c()
+    cls()
     sys.exit(0)
 
 signal.signal(signal.SIGINT,signal_handler)
--- utils/shortcuts.py
+++ utils/shortcuts.py
@@ -1,21 +1,11 @@
-# 导入必要组件
-import sys
-import time
-from os import system
-import win32gui
-import win32con
-import win32api
-import win32gui_struct
-from pynput import keyboard
-from ctypes import *
-import signal
-import playsound
+from utils.imports import *
 
-r = system
+def run(inputCommand):
+    system(inputCommand)
 
-def c():
-    r("cls")
+def cls():
+    run("cls")
 
-def wait():
-    time.sleep(2)
+def wait(waitTime):
+    time.sleep(waitTime)
     
